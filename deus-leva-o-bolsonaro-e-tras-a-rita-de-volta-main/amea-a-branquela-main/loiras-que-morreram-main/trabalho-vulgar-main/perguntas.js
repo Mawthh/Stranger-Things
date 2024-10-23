ad64ad09cc30ca714d280c05ffaf487b746b7878
time to say goodnight <133375865+Mawthh@users.noreply.github.com>
2024-10-23 15:03:07 -0300

    Add files via upload

---
+++ deus-leva-o-bolsonaro-e-tras-a-rita-de-volta-main/amea-a-branquela-main/loiras-que-morreram-main/trabalho-vulgar-main/perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    '1° Temp',
+    'Qual o nome da careca de Stranger things?',
+    '- Eleven -'
+    )
+    criaCartao(
+    '3° Temp',
+    'Qual o nome do irmão morto da Max?',
+    '- Billy Williams -'
+    )
+    criaCartao(
+    '4° Temp',
+    'Quantas loiras o Vecna matou?',
+    '- 3 -'
+    )
